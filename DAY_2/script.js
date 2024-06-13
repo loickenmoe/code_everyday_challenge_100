c77a76aa9e4cb445aa8a3c1d87b811897e00dbfc
loic kenmoe <loickenmoe1@gmail.com>
2024-06-13 12:59:31 +0100

    commit to DAY-2

---
+++ DAY_2/script.js
@@ -0,0 +1,47 @@
+const progress = document.getElementById("progress")
+const circles = document.querySelectorAll(".circle")
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+
+let currrentActive = 1;
+
+next.addEventListener("click", () => {
+  currrentActive++;
+  if (currrentActive > circles.length) {
+    currrentActive = circles.length
+  }
+  update()
+})
+
+
+prev.addEventListener("click", () => {
+  currrentActive--;
+  if (currrentActive < 1) {
+    currrentActive = 1
+  }
+  update()
+})
+
+
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currrentActive) {
+      circle.classList.add("active")
+    } else {
+      circle.classList.remove("active")
+    }
+  })
+
+  const actives = document.querySelectorAll(".active");
+  progress.style.width = (actives.length - 1 )/ (circles.length - 1) * 100 + '%'
+
+  if(currrentActive === 1 ){
+    prev.disabled = true
+  }else if (currrentActive === circles.length) {
+    next.disabled = true
+  } else {
+    prev.disabled = false
+    next.disabled = false
+  }
+}
