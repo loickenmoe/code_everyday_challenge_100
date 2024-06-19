7e2e4e8ab6a35972516b36ffb37268c32ddb47d8
loic kenmoe <loickenmoe1@gmail.com>
2024-06-19 13:38:01 +0100

    Release_V1 DAY9: Sound Board

---
+++ DAY_9/script.js
@@ -0,0 +1,23 @@
+const sounds = ["audio1", "audio2", "audio3", "audio4", "audio5", "audio6"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
